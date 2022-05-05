023395f9beb8d4ccac69e13ba04ad58d6ade2b42
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2022-05-05 16:30:59 -0300

    estructura de html armada, comenzando con los estilos de css para luego pasar a js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 window.addEventListener('load', () => {
+    const form = document.forms[0];
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        
+
+
+
+
+    })
 
 
 
