6f74c51fcdb7e1b44e6632b12b11c35763895288
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2022-05-08 23:54:32 -0300

    logica para renderizar los montos e ir actualizandolos segun se vayan cambiando los datos ingresados

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ window.addEventListener('load', () => {
     const regex2 = /^[0-9]+$/
 
     /* Constantes */
-    const dos = document.querySelector("#dos")
     const form = document.forms[0]
     const montoTotal = document.querySelector("#montoTotal")
     const cantidadPersonas = document.querySelector("#cantidadPersonas")
@@ -14,14 +13,18 @@ window.addEventListener('load', () => {
     const custom = document.querySelector("#custom")
     const reset = document.querySelector("#reset")
     const montoAPagar = document.querySelectorAll(".montoAPagar")
+    const totalPorPersona = document.querySelector("#totalPorPersona")
+    const propinaPorPersona= document.querySelector("#propinaPorPersona")
     const errorMonto = document.querySelector("#errorMonto")
     const errorPersonas = document.querySelector("#errorPersonas")
     const arrayConPunto = []
     const arraySinPunto = []
     const maxPersonas = []
+    const maxPropina = []
     let valorMontoTotal = 0
     let valorCantidadDePersonas = 0
-    let eleccionPropina = 0
+    let propinaAPagar = 0
+    let porcentaje = 0
 
     /* Prevenir enviado del form */
     form.addEventListener('submit', (e) => {
@@ -51,9 +54,9 @@ window.addEventListener('load', () => {
     /* la visualizacion del error */
     montoTotal.addEventListener("blur", () => {
         validarMonto()
-        validarReset()
+        renderizar()
     })
-    
+
     /* Validacion para el ingreso de la cantidad de personas */
     /* No permitiendo ser 0 y maximo de 2 digitos */
     cantidadPersonas.addEventListener("keydown", (e) => {
@@ -70,29 +73,29 @@ window.addEventListener('load', () => {
     /* para la visualizacion del error */
     cantidadPersonas.addEventListener("blur", () => {
         validarTotalPersonas()
-        validarReset()
-     })
+        renderizar()
+    })
     
     /* Cambio de estilo para la propina elegida */
     /* Y enviar valor de la propina a la funcion encargada */
-    propinas.forEach( (e) => {
+    propinas.forEach((e) => {
         e.addEventListener("click", () => {
             propinas.forEach(e=>e.classList.remove("propinaElegida"));
             e.classList.add("propinaElegida")
             valorPropina(e.innerHTML)
-            validarReset()
+            renderizar()
         })
     })
-
+    
     /* Logica del boton de custom de la propina */
     /* Y validaciones */
     custom.addEventListener("keydown", (e) => {
         if (e.code === "Backspace") {
-            maxPersonas.pop()
+            maxPropina.pop()
         } 
     })
     custom.addEventListener("keypress", (e) => {
-        if (!validarPersonas(e.key)) {
+        if (!validarCustom(e.key)) {
             e.preventDefault()
         }
     })
@@ -100,10 +103,12 @@ window.addEventListener('load', () => {
         if (custom.value == 0){
             alert("Custom tip can't be zero")
         } else {
-            eleccionPropina = custom.value
-            validarReset()
+            let valor = custom.value * valorMontoTotal / 100
+            propinaAPagar = valor / valorCantidadDePersonas
+            renderizar()
         }
     })
+    
 
     /* Funcion de validacion de numero en monto total */
     function validarTotal (num) {
@@ -133,59 +138,84 @@ window.addEventListener('load', () => {
         } else false 
     }
 
+    /* Validar propina personalizada */
+    function validarCustom (cant) {
+        let p = cant.match(regex2)
+
+        if (p && maxPropina.length < 2) {
+            maxPropina.push(cant)
+            return true
+        } else false 
+    }
+
     /* Funcion para obtener el porcentaje de la propina */
     /* y cantidad a pagar segun cantidad de personas */
     function valorPropina (eleccion) {
         let valor = eleccion.trim()
         let num = valor.slice(0, -1)
-        let montoPropina = num * valorMontoTotal / 100
-        eleccionPropina = montoPropina / valorCantidadDePersonas
-        console.log(eleccionPropina);
+        porcentaje = num
+        propinaAPagar = num * valorMontoTotal / 100
     }
 
+    /* Envia el valor de la nueva propina al cambiar el monto */
+    function nuevaPropina () {
+        propinaAPagar = porcentaje * valorMontoTotal / 100
+    }
+
+    /* Funcion para renderizar los montos de propina y total */
+    function renderizar () {
+        nuevaPropina ()
+        if (valorMontoTotal !== 0 && valorCantidadDePersonas !== 0 && propinaAPagar !== 0) {
+            validarReset()
+            propinaPorPersona.innerHTML = `$${propinaAPagar / valorCantidadDePersonas}`
+            totalPorPersona.innerHTML = `$${(valorMontoTotal + propinaAPagar) / valorCantidadDePersonas}`
+        }
+    }
+    
+    /* Funcion para validar el boton de reset */
+    function validarReset () {
+        montoAPagar.forEach(e => {
+            let monto = e.innerHTML
+            let recorte = monto.slice(1)
+            let valor = parseInt(recorte)
+            if(valor !== 0.00){
+                reset.removeAttribute("disabled")
+            } else if (valor === 0.00){
+                reset.setAttribute("disabled", true)
+            }
+        })
+    }
+    
     /* Errores */
 
-    /* Validacion en los montos */
+    /* Visualizar error en el monto */
+    /* Mas borde de error */
     function validarMonto () {
-        if (montoTotal.value == 0){
+        if (montoTotal.value === 0){
             errorMonto.classList.remove("hide")
             errorMonto.classList.add("errorPropina")
+            montoTotal.classList.add("errorBorde")
         } else {
             errorMonto.classList.remove("errorPropina")
             errorMonto.classList.add("hide")
-            valorMontoTotal = montoTotal.value
+            montoTotal.classList.remove("errorBorde")
+            valorMontoTotal = parseInt(montoTotal.value)
         }
     }
 
-    /* Validacion en las personas */
+    /* Visualizar error en las personas */
+    /* Mas borde de error */
     function validarTotalPersonas () {
         if (cantidadPersonas.value == 0){
             errorPersonas.classList.remove("hide")
             errorPersonas.classList.add("errorPersona")
+            cantidadPersonas.classList.add("errorBorde")
          } else {
             errorPersonas.classList.remove("errorPersona")
             errorPersonas.classList.add("hide")
+            cantidadPersonas.classList.remove("errorBorde")
             valorCantidadDePersonas = cantidadPersonas.value
         }
     }
-
-    /* Funcion para validar el boton de reset */
-    function validarReset () {
-        if (valorMontoTotal !== 0 && valorCantidadDePersonas !== 0 && eleccionPropina !== 0) {
-            montoAPagar.forEach(e => {
-                let monto = e.innerHTML
-                let recorte = monto.slice(1)
-                let valores = parseInt(recorte)
-                if(valores !== 0.00){
-                    reset.removeAttribute("disabled")
-                } else if (valores === 0.00){
-                    reset.setAttribute("disabled")
-                }
-            })
-        }
-    }
-
-
-
     
 })
